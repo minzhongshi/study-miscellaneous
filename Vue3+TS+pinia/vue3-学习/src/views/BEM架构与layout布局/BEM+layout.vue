96722c4bf589ed6ca92f623f7bc19da7de2130ac
史明忠 <2240941938@qq.com>
2023-08-14 18:27:19 +0800

    vue3生命周期 BEM架构 layout布局

---
+++ Vue3+TS+pinia/vue3-学习/src/views/BEM架构与layout布局/BEM+layout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+/**
+ * layout布局：左侧位菜单 右侧有头部 头部下面为内容
+ * BEM架构：css架构 （block elements modify） 样式规范 例如：
+ * el-input :el 为名字前缀 input为整个块
+ * el-input__wrapper : __wrapper 为块内元素 ===> e
+ * el-button--primary : --primary 为修饰词 ===> m
+ */
+
+
+</style>
