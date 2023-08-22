8e8285640e2a03186945ce1409d1f7c6fdf75762
史明忠 <2240941938@qq.com>
2023-08-23 04:53:06 +0800

    1.全局组件 2.递归组件 3.动态组件 4.异步组件，JS打包分包视频观看

--- Vue3+TS+pinia/vue3-学习/src/App.vue
+++ Vue3+TS+pinia/vue3-学习/src/App.vue
@@ -9,10 +9,88 @@ import Ref from "./views/vue3_响应式/ref全家桶/ref.vue";
 import AntiShake from "./views/vue3_响应式/ref全家桶/customRef实现防抖/antiShake.vue";
 import Reactive from "./views/vue3_响应式/reactive全家桶/reactive.vue";
 import BEM_layout from "./views/BEM架构与layout布局/BEM_layout.vue";
-import SzieDireect from "./views/指令/szieDireect.vue";
+import SzieDireect from "./views/自定义指令/监听宽高指令v-size-ob/szieDireect.vue";
 import ProvideInjectTest from "./views/组件通讯/07_provide-inject/ProvideInjectTest.vue";
 import Index from "./views/组件通讯/08_pinia/index.vue";
 import SlotTest from "./views/组件通讯/09_slot/SlotTest.vue";
+import Card from "./components/组件/全局组件/Card.vue";
+import Tree from "./components/组件/递归组件/Tree.vue";
+import {markRaw, reactive, ref, shallowRef} from "vue";
+import A from "./components/组件/动态组件/A.vue";
+import B from "./components/组件/动态组件/B.vue";
+import C from "./components/组件/动态组件/C.vue";
+
+// 递归组件
+interface Tree {
+  name: string,
+  checked: boolean,
+  children?: Tree[]
+}
+const data = reactive<Tree[]>([
+  {
+    name: '1',
+    checked: false,
+    children: [
+      {
+        name: '1-1',
+        checked: true,
+      }
+    ]
+  },
+  {
+    name: '2',
+    checked: false,
+  },
+  {
+    name: '3',
+    checked: false,
+    children: [
+      {
+        name: '3-1',
+        checked: false,
+        children: [
+          {
+            name: '3-1-1',
+            checked: false,
+            children: [
+              {
+                name: '3-1-1-1',
+                checked: false,
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: '3-2',
+        checked: false,
+      }
+    ]
+  }
+])
+
+//动态组件
+const data2 = reactive([
+  {
+    name: 'A组件',
+    com: markRaw(A)
+  },
+  {
+    name: 'B组件',
+    com: markRaw(B)
+  },
+  {
+    name: 'C组件',
+    com: markRaw(C)
+  }
+])
+const comId= shallowRef(A)
+const active = ref(0)
+const switchCom =(item,index)=>{
+  comId.value = item.com
+  active.value = index
+}
+
 </script>
 
 <template>
@@ -29,8 +107,25 @@ import SlotTest from "./views/组件通讯/09_slot/SlotTest.vue";
 <!--  <SzieDireect/>-->
 <!--  <ProvideInjectTest/>-->
 <!--  <Index/>-->
-  <SlotTest/>
+<!--  <SlotTest/>-->
+<!--  <Card/>-->
+<!--    <Tree :data="data"></Tree>-->
+    <div style="display: flex" >
+      <div @click="switchCom(item,index)" :class="[active === index ? 'active': '']" class="tabs" v-for="(item,index) in data2">
+        <div>{{item.name}}</div>
+      </div>
+    </div>
+    <component :is="comId"></component>
 </template>
 
 <style scoped>
+.active {
+  background-color: #6fc3df;
+}
+.tabs{
+  border: 1px solid #ccc;
+  padding: 5px 10px;
+  margin-left: 5px;
+  cursor: pointer;
+}
 </style>
--- Vue3+TS+pinia/vue3-学习/src/main.ts
+++ Vue3+TS+pinia/vue3-学习/src/main.ts
@@ -8,10 +8,17 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import sizeDireect from '../src/directs/resize指令封装/sizeDireect'
 // pinia
 import store from "./store";
+// 全局挂载组件
+// @ts-ignore
+import Card from './components/组件/全局组件/Card.vue';
 
 const app = createApp(App)
+// 循环挂载Icon图标
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
+//挂载组件
+app.component('Card',Card)
+//挂载指令，省略‘v-’前缀
 app.directive('size-ob', sizeDireect)
 app.use(store).use(ElementPlus).mount('#app')
